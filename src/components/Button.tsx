d8d85942b121114e50a3827af9e2e1cf951436fd
sabit1997 <yejiii1997@gmail.com>
2023-05-18 15:41:12 +0900

    Feat: Button 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+const primary = css`
+  background-color: var(--blue-purple);
+  color: #000;
+`;
+
+const purple = css`
+  color: var(--white);
+  background-color: var(--blue-purple);
+`;
+
+const white = css`
+  border: 1px solid var(--blue-purple);
+  background-color: var(--white);
+  color: #000;
+`;
+
+const wideBig = css`
+  width: 100%;
+  height: 45px;
+`;
+
+const big = css`
+  width: 206px;
+  height: 47px;
+`;
+
+const mid = css`
+  width: 119px;
+  height: 50px;
+`;
+
+const small = css`
+  width: 85px;
+  height: 26px;
+`;
+
+const colors = { primary, purple, white };
+const sizes = { wideBig, big, mid, small };
+
+interface PropsTypes extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color: 'primary' | 'purple' | 'white';
+  size: 'wideBig' | 'big' | 'mid' | 'small';
+}
+
+const ButtonStyle = styled.button<PropsTypes>`
+  font-size: 1.6rem;
+  border-radius: 5px;
+  cursor: pointer;
+  ${(props) => (props.color ? colors[props.color] : '')}
+  ${(props) => (props.size ? sizes[props.size] : '')}
+`;
+
+const Button: React.FC<PropsTypes> = ({ children, ...props }) => {
+  return <ButtonStyle {...props}>{children}</ButtonStyle>;
+};
+
+export default Button;
