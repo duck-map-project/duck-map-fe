44e2cb3a6ea624439f5e9ae15d14d40159d82b72
shaykk <kimmshay@gmail.com>
2023-05-19 19:56:19 +0900

    Feat: textbox 수정

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -5,47 +5,23 @@ interface TextProps {
 }
 
 const TextWrapper = styled.div`
-  position: absolute;
-  width: 500px;
-  height: 50px;
-  left: 747px;
-  top: 88px;
-`;
-
-const TextBoxInner = styled.div`
   box-sizing: border-box;
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-  background: #FFFFFF;
-  border: 1px solid #B8C0FF;
+  background: #ffffff;
+  border: 1px solid #b8c0ff;
   border-radius: 5px;
-`;
-
-const Content = styled.span`
-  position: absolute;
-  left: 45%;
-  right: 45.22%;
-  top: 26%;
-  bottom: 34%;
-
   font-family: 'Abhaya Libre Medium';
-  font-style: normal;
   font-weight: 500;
   font-size: 16px;
-  line-height: 19px;
-
+  line-height: 50px;
+  max-width: 500px;
   color: #000000;
+  text-align: center;
 `;
 
 const TextBox = ({ text }: TextProps) => {
   return (
     <TextWrapper>
-      <TextBoxInner>
-        <Content>{text}</Content>
-      </TextBoxInner>
+      {text}
     </TextWrapper>
   );
 };
