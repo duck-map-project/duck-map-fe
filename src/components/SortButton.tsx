ba8a907fff1a9babee8ec270b3f228e400b99f08
sabit1997 <yejiii1997@gmail.com>
2023-05-21 14:16:27 +0900

    Feat: SortButton 컴포넌트 추가
    Refs: #3

---
+++ src/components/SortButton.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+
+import arrowDown from '../assets/dropdown-arrow-down.svg';
+import arrowUp from '../assets/dropdown-arrow-up.svg';
+
+import Dropdown from './Dropdown';
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Select = styled.button<{ clicked: boolean }>`
+  width: 180px;
+  height: 50px;
+  font-size: 1.6rem;
+  border: 1px solid var(--purple);
+  border-radius: 5px;
+  background-image: ${({ clicked }) =>
+    clicked ? `url(${arrowUp})` : `url(${arrowDown})`};
+  background-repeat: no-repeat;
+  background-position: right 30px center;
+`;
+
+const SortDropdown = () => {
+  const [clicked, setClicked] = useState(false);
+  const sortOption = ['인기순', '리뷰많은순'];
+  const [SelectedText, setSelectedText] = useState(sortOption[0]);
+
+  const handleClick = () => {
+    if (clicked) {
+      setClicked(false);
+    } else {
+      setClicked(true);
+    }
+  };
+  return (
+    <Wrapper>
+      <Select clicked={clicked} onClick={handleClick}>
+        {SelectedText}
+      </Select>
+      {clicked ? (
+        <Dropdown
+          lists={sortOption}
+          setSelectedText={setSelectedText}
+          setClicked={setClicked}
+        />
+      ) : null}
+    </Wrapper>
+  );
+};
+
+export default SortDropdown;
