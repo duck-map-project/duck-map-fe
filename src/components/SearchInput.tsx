e38fd63ec31ee067b6a8ded578f014a0eea3f06d
shaykk <kimmshay@gmail.com>
2023-06-01 03:12:08 +0900

    Feat: searchinput component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,39 @@
+import { styled } from 'styled-components';
+
+import iconSearch from '../assets/icon-edit.svg';
+
+export const SearchInputBox = styled.div`
+position: relative;
+width: 100%;
+`;
+
+const SearchSection = styled.input`
+border: 1px solid #C8B6FF;
+border-radius: 0.5rem;
+padding: 10px;
+width: 500px;
+`;
+
+const Icon = styled.p`
+position: absolute;
+top: 8px;
+left: 10px;
+background-image: url(${iconSearch});
+background-repeat: no-repeat;
+background-size: contain;
+width: 25px;
+height: 25px;
+margin-right:5px;
+`;
+
+const SearchInput = () => {
+  return (
+    <SearchInputBox>
+      <SearchSection>
+      </SearchSection>
+      <Icon />
+    </SearchInputBox>
+  );
+};
+
+export default SearchInput;
