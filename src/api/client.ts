7d5ac275a5b183be7ee75756c9749b6c77f593ce
sabit1997 <yejiii1997@gmail.com>
2023-06-02 01:55:21 +0900

    Fix: 불필요한 코드 지우기 -proxy 설정으로 url 불필요해짐

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,3 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
-const baseURL = process.env.BASE_URL;
-
-export const client: AxiosInstance = axios.create({
-  baseURL: baseURL,
-});
+export const client = axios.create({});
