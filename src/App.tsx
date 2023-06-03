9e0c1c863ca9f289980fa8e630cd517a0fc4f0b4
sabit1997 <yejiii1997@gmail.com>
2023-06-03 21:35:03 +0900

    Feat: AuthContext 추가
    Refs: #54

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { AuthProvider } from './contexts/AuthContext';
 import { routers } from './router';
 import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <GlobalStyle />
       <RouterProvider router={routers} />
-    </>
+    </AuthProvider>
   );
 }
 
