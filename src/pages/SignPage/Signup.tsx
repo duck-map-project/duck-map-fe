9911f633fc68a9e594a240782e42c90df26c090c
sabit1997 <yejiii1997@gmail.com>
2023-06-03 20:30:46 +0900

    Fix: 이미지 업로드 화면 추가 및 usePreviewImage hook 적용
    Refs: close #49

--- src/pages/SignPage/Signup.tsx
+++ src/pages/SignPage/Signup.tsx
@@ -1,7 +1,9 @@
 import kakaotalk from '../../assets/kakaotalk.svg';
 import twitter from '../../assets/twitter-circle.svg';
+import AddImageButton from '../../components/AddImageButton';
 import AuthInput from '../../components/AuthInput';
 import Button from '../../components/Button';
+import usePreviewImage from '../../hooks/usePreviewImage';
 
 import {
   PageWrapper,
@@ -10,13 +12,27 @@ import {
   SNSSignupText,
   SNSButtonWrapper,
   SNSSignupButton,
+  ProfileImage,
+  ProfileInput,
 } from './SignStyle';
 
 const Signup = () => {
+  const { previewImage, onChange } = usePreviewImage(null);
+  console.log(previewImage);
+
   return (
     <PageWrapper>
       <PageTitle>회원가입</PageTitle>
       <Form>
+        <ProfileInput
+          id="profile-input"
+          type="file"
+          accept="image/*"
+          onChange={onChange}
+        />
+        <ProfileImage image={previewImage}>
+          <AddImageButton size="mid" htmlFor="profile-input" />
+        </ProfileImage>
         <AuthInput name="email" title="이메일" type="email" />
         <AuthInput name="password" title="비밀번호" type="password" />
         <AuthInput
