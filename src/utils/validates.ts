08b15390d057982d7979bcae239e7bf1c68304b8
sabit1997 <yejiii1997@gmail.com>
2023-06-06 22:08:19 +0900

    Fix: 에러 문구 수정

--- src/utils/validates.ts
+++ src/utils/validates.ts
@@ -16,27 +16,27 @@ export const validateEmail = (email: string): string | undefined => {
   if (email === '') {
     return '필수 항목입니다.';
   } else if (!/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email)) {
-    return '유효한 이메일 주소가 아닙니다.';
+    return '이메일 형식이 아닙니다.';
   }
   return undefined;
 };
 
 export const validatePassword = (password: string): string | undefined => {
   if (password === '') {
-    return '비밀번호는 필수 항목입니다.';
+    return '필수 항목입니다.';
   } else if (
     !/(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,16}/.test(password)
   ) {
-    return '유효하지 않은 비밀번호입니다.';
+    return '영대소문자, 숫자, 특수문자를 포함한 8 ~ 16자여야 합니다.';
   }
   return undefined;
 };
 
 export const validateUsername = (username: string): string | undefined => {
   if (username === '') {
-    return '사용자 이름은 필수 항목입니다.';
+    return '필수 항목입니다.';
   } else if (!/^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}/.test(username)) {
-    return '유효하지 않은 사용자 이름입니다.';
+    return '한글, 영문 소문자, 숫자, -,_ 로 이루어진 2 ~ 10자여야 합니다.';
   }
   return undefined;
 };
