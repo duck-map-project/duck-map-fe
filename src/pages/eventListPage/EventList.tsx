89770428cf94553ffb0478426157cd65416362aa
sabit1997 <yejiii1997@gmail.com>
2023-06-17 22:59:52 +0900

    Feat: 이벤트 목록 렌더링 및 무한 스크롤 적용
    close #106

--- src/pages/eventListPage/EventList.tsx
+++ src/pages/eventListPage/EventList.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
+
+import { eventsApi } from '../../api/event';
 import ChoiceStarBar from '../../components/ChoiceStarBar';
 import EventListItem from '../../components/EventListItem';
-import feedData from '../MyPage/feedData.json';
+import { EventData, getEventParams } from '../../types/eventService';
 
 import {
   ListContentsSection,
@@ -12,6 +15,50 @@ import {
 } from './EventListStyle';
 
 const EventList = () => {
+  const [events, setEvents] = useState<EventData[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [isLast, setIsLast] = useState<boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    if (scrollHeight - scrollTop === clientHeight && !isFetching && !isLast) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      setIsFetching(true);
+      const eventParams: getEventParams = {
+        artistId: 0,
+        inProgress: true,
+        page: page,
+        size: 2,
+        sort: ['sorted'],
+      };
+      const res = await eventsApi.get(eventParams);
+      setIsFetching(false);
+      setIsLast(res.last);
+      setEvents((prevEvents) => [...prevEvents, ...res.content]);
+    } catch (error) {
+      console.error(error);
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    if (page >= 0 && !isLast) {
+      fetchEvents();
+    }
+  }, [page]);
+
   return (
     <PageWrapper>
       <ChoiceStarBar />
@@ -22,18 +69,10 @@ const EventList = () => {
         {/* TODO: 클릭한 연예인 이름 가져오기 */}
         <EventTitle>(이름) 이벤트</EventTitle>
         <Ul>
-          {feedData.map((feed, key) => (
-            <EventListItem
-              key={key}
-              groupName={feed.groupName}
-              member={feed.member}
-              address={feed.address}
-              eventImg={feed.eventImg}
-            />
-          ))}
-          {/* <EventListItem /> */}
-          {/* <EventListItem /> */}
-          {/* <EventListItem /> */}
+          {events &&
+            events.map((event) => (
+              <EventListItem event={event} key={event.id} />
+            ))}
         </Ul>
       </ListContentsSection>
     </PageWrapper>
