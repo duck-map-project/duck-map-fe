4d3485e3144ba1f50b2a8760401e0a1c91cf28e6
vnfdusdl <rlachdus2@naver.com>
2023-07-08 22:12:13 +0900

    Feat: icon 이미지 png->svg 변경 및 font resolve error 해결

--- src/styles/font.tsx
+++ src/styles/font.tsx
@@ -2,8 +2,8 @@ import { css } from 'styled-components';
 
 import RobotoBold from '../fonts/Roboto-Bold-webfont.woff';
 import RobotoRegular from '../fonts/Roboto-Regular-webfont.woff';
-// import SuiteBold from '../fonts/SUITE-Bold.woff'
-// import SuiteRegular from '../fonts/SUITE-Regular.woff';
+import SuiteBold from '../fonts/SUITE-Bold.woff2';
+import SuiteRegular from '../fonts/SUITE-Regular.woff2';
 
 const font = css`
   @font-face {
@@ -20,13 +20,13 @@ const font = css`
   }
   @font-face {
     font-family: 'Suite';
-    src: url(../fonts/SUITE-Bold.woff2) format('woff2');
+    src: url(${SuiteBold}) format('woff2');
     font-style: normal;
     font-weight: 700;
   }
   @font-face {
     font-family: 'Suite';
-    src: url(../fonts/SUITE-Regular.woff2) format('woff2');
+    src: url(${SuiteRegular}) format('woff2');
     font-style: normal;
     font-weight: 400;
   }
