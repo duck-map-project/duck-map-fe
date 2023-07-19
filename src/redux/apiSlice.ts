827d0e024bf43dc10dcaf567b6880be5fcb9ed38
sabit1997 <yejiii1997@gmail.com>
2023-07-19 21:53:31 +0900

    Feat: baseURL 환경변수로 지정
    close #167

--- src/redux/apiSlice.ts
+++ src/redux/apiSlice.ts
@@ -1,12 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const baseUrl = process.env.REACT_APP_BASE_URL;
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    //cors 에러 처리 후 proxy 삭제 시 활성화
-    // baseUrl,
+    baseUrl,
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
