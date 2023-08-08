b2d6868c677a277028baf7aef7928b66deda08a3
sabit1997 <yejiii1997@gmail.com>
2023-08-08 13:41:51 +0900

    Fix: 이미지 슬라이더 컴포넌트 type props로 조건부 스타일링
    Refs: #249

--- src/pages/DetailInfoPage/ImageSlider.tsx
+++ src/pages/DetailInfoPage/ImageSlider.tsx
@@ -1,73 +1,126 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
-const SliderContainer = styled.div`
-  width: 416px;
+import leftTriangle from '../../assets/left-triangle.svg';
+import rightTriangle from '../../assets/right-triangle.svg';
+import whiteLeftArrow from '../../assets/white-left-arrow.svg';
+import whiteRightArrow from '../../assets/white-right-arrow.svg';
+
+const primarySize = css`
+  width: 400px;
   height: 357px;
+`;
+
+const reviewSize = css`
+  width: 438px;
+  height: 410px;
+`;
+
+const SliderContainer = styled.div<{ type: 'review' | 'primary' }>`
+  ${(props) => (props.type === 'review' ? reviewSize : primarySize)}
   border: 2px solid #1e232c;
+  background-color: #fff;
   overflow: hidden;
   position: relative;
+  border-radius: ${(props) => (props.type === 'review' ? '20px' : '')};
+  position: ${(props) => (props.type === 'review' ? 'relative' : '')};
 `;
 
-const SliderTrack = styled.div<{ transform: number }>`
+const SliderTrack = styled.div<{
+  transform: number;
+  type: 'review' | 'primary';
+}>`
   display: flex;
+  position: relative;
   transition: transform 0.3s ease;
-  transform: ${(props) => `translateX(-${props.transform * 416}px)`};
+  transform: ${(props) =>
+    props.type === 'review'
+      ? `translateX(-${props.transform * 438}px)`
+      : `translateX(-${props.transform * 400}px)`};
 `;
 
-const Slide = styled.img`
+const Slide = styled.img<{ type: 'review' | 'primary' }>`
   flex-shrink: 0;
-  width: 416px;
-  height: 357px;
+  ${(props) => (props.type === 'review' ? reviewSize : primarySize)}
 `;
 
 const SlideButton = styled.button`
-  padding: 30px 10px;
-  background-color: #d8caff;
-  font-size: 1.8rem;
-  font-weight: 700;
-  line-height: 1.247777777777778;
-  border: none;
-  cursor: pointer;
-  outline: none;
+  width: 12px;
+  height: 20px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
 `;
 
 const LeftSlideButton = styled(SlideButton)`
-  left: 0;
-  border-radius: 0 50% 50% 0;
+  background-image: url(${leftTriangle});
+  left: -20px;
 `;
 
 const RightSlideButton = styled(SlideButton)`
-  right: 0;
-  border-radius: 50% 0 0 50%;
+  background-image: url(${rightTriangle});
+  right: -20px;
 `;
 
-const DotContainer = styled.div`
+const ReviewSlideButton = styled.button`
+  width: 10px;
+  height: 20px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  bottom: 11px;
+`;
+
+const ReviewRightButton = styled(ReviewSlideButton)`
+  background-image: url(${whiteRightArrow});
+  right: 16px;
+`;
+
+const ReviewLeftButton = styled(ReviewSlideButton)`
+  background-image: url(${whiteLeftArrow});
+  left: 16px;
+`;
+
+const DotContainer = styled.div<{ type: 'review' | 'primary' }>`
   display: flex;
   justify-content: center;
+  gap: ${(props) => (props.type === 'review' ? '19.25px' : '14px')};
   margin-top: 8px;
   position: absolute;
   bottom: 13px;
   left: 50%;
   transform: translateX(-50%);
 `;
-
-const Dot = styled.div<{ active: boolean }>`
+const primary = css`
   width: 8px;
   height: 8px;
+`;
+
+const reviewDot = css`
+  width: 14px;
+  height: 14px;
+  border: 2px solid #1e232c;
+`;
+
+const Dot = styled.div<{ active: boolean; type: 'review' | 'primary' }>`
+  ${(props) => (props.type === 'review' ? reviewDot : primary)}
   border-radius: 50%;
-  margin: 0 4px;
   background-color: ${(props) => (props.active ? '#FE83AF' : '#F8F8FA')};
 `;
 
 interface ImageSliderProps {
   images: string[];
+  type?: 'review' | 'primary';
 }
 
-const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+const ImageSlider: React.FC<ImageSliderProps> = ({
+  images,
+  type = 'primary',
+}) => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const nextSlide = () => {
@@ -82,32 +135,51 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
     );
   };
 
-  useEffect(() => {
-    if (images && images.length > 0) {
-      const slideInterval = setInterval(nextSlide, 3000);
-      return () => clearInterval(slideInterval);
-    }
-  }, [images]);
-
   if (!images || images.length === 0) {
     return null;
   }
 
   return (
-    <SliderContainer>
-      <SliderTrack transform={currentSlide}>
-        {images.map((image, index) => (
-          <Slide key={index} src={image} alt={`Slide ${index + 1}`} />
-        ))}
-      </SliderTrack>
-      <LeftSlideButton onClick={prevSlide}>Prev</LeftSlideButton>
-      <RightSlideButton onClick={nextSlide}>Next</RightSlideButton>
-      <DotContainer>
-        {images.map((_, index) => (
-          <Dot key={index} active={index === currentSlide} />
-        ))}
-      </DotContainer>
-    </SliderContainer>
+    <>
+      <SliderContainer type={type}>
+        <SliderTrack transform={currentSlide} type={type}>
+          {images.map((image, index) => (
+            <Slide
+              key={index}
+              src={image}
+              alt={`Slide ${index + 1}`}
+              type={type}
+            />
+          ))}
+        </SliderTrack>
+        {type === 'primary' ? (
+          <DotContainer type={type}>
+            {images.map((_, index) => (
+              <Dot key={index} active={index === currentSlide} type={type} />
+            ))}
+          </DotContainer>
+        ) : null}
+        {type === 'review' ? (
+          <>
+            <ReviewLeftButton type="button" onClick={prevSlide} />
+            <ReviewRightButton type="button" onClick={nextSlide} />
+          </>
+        ) : null}
+      </SliderContainer>
+      {type === 'review' ? (
+        <DotContainer type={type}>
+          {images.map((_, index) => (
+            <Dot key={index} active={index === currentSlide} type={type} />
+          ))}
+        </DotContainer>
+      ) : null}
+      {type === 'primary' ? (
+        <>
+          <LeftSlideButton type="button" onClick={prevSlide} />
+          <RightSlideButton type="button" onClick={nextSlide} />
+        </>
+      ) : null}
+    </>
   );
 };
 
