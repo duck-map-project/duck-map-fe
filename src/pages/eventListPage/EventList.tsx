dc08cfb213eff97ffab4d22e535ca593f63fc813
sabit1997 <yejiii1997@gmail.com>
2023-08-08 20:55:52 +0900

    Feat: 이벤트 목록 페이지 좋아요 설정 / 삭제 기능 추가
    close #259

--- src/pages/eventListPage/EventList.tsx
+++ src/pages/eventListPage/EventList.tsx
@@ -3,10 +3,16 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 
 import ChoiceArtistBar from '../../components/ChoiceArtistBar';
-import EventListItem from '../../components/EventListItem';
+import EventListItem, {
+  HandleLikeClickProps,
+} from '../../components/EventListItem';
 import KakaoMap from '../../components/KakaoMap';
 import AddEventModal from '../../components/modals/AddEventModal';
-import { useGetEventQuery } from '../../redux/eventApiSlice';
+import {
+  useAddLikeMutation,
+  useDeleteLikeMutation,
+  useGetEventQuery,
+} from '../../redux/eventApiSlice';
 import { setPlace } from '../../redux/eventPlaceSlice';
 import {
   selectEventArtist,
@@ -46,6 +52,8 @@ const EventList = () => {
     ...(selectedGroup &&
       !selectedArtist && { artistId: selectedGroup.id.toString() }),
   });
+  const [addLike] = useAddLikeMutation();
+  const [deleteLike] = useDeleteLikeMutation();
   const scrollAreaRef = useRef<HTMLDivElement>(null);
   const scrollArea = scrollAreaRef.current;
   const dispatch = useDispatch();
@@ -106,11 +114,31 @@ const EventList = () => {
     setAddEventModal(false);
   };
 
+  const handleLikeClick = async ({ eventId, likeId }: HandleLikeClickProps) => {
+    if (likeId) {
+      try {
+        await deleteLike(likeId).unwrap();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        await addLike(eventId.toString());
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   let content;
 
   if (events) {
     content = events.map((event) => (
-      <EventListItem event={event} key={event.id} />
+      <EventListItem
+        event={event}
+        key={event.id}
+        handleLikeClick={handleLikeClick}
+      />
     ));
   } else if (isLoading) {
     content = <div>이벤트 목록을 불러오는 중입니다</div>;
@@ -119,7 +147,6 @@ const EventList = () => {
   } else {
     content = <div>이벤트가 없습니다</div>;
   }
-
   return (
     <PageWrapper>
       <ChoiceArtistBar />
