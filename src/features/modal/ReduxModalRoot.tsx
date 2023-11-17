823f7f4bfe33b1eb0d781be760522eee94f1df80
vnfdusdl <rlachdus2@naver.com>
2023-11-17 19:12:02 +0900

    Feat: 변수 modals 선언 - 모달 컴포넌트를 한 곳에서 관리해주기 위해 생성 Refs: #335

--- src/features/modal/ReduxModalRoot.tsx
+++ src/features/modal/ReduxModalRoot.tsx
@@ -1,6 +1,29 @@
 import { useSelector } from 'react-redux';
 
 import useModal from '../../hooks/useModal';
+import ArtistModal from '../artists/modals/ArtistModal';
+import ArtistSelectModal from '../artists/modals/ArtistSelectModal';
+import ArtistTypeModal from '../artists/modals/ArtistTypeModal';
+import EventArtistModal from '../artists/modals/EventArtistModal';
+import GroupModal from '../artists/modals/GroupModal';
+import BookmarkFolderModal from '../bookmarks/modals/BookmarkFolderModal';
+import BookmarkModal from '../bookmarks/modals/BookmarkModal';
+import CategoryModal from '../categories/modals/CategoryModal';
+import CategorySelectModal from '../categories/modals/CategorySelectModal';
+import AddressSearchModal from '../events/modals/AddressSearchModal';
+
+export const modals = {
+  artistModal: ArtistModal,
+  artistTypeModal: ArtistTypeModal,
+  artistSelectModal: ArtistSelectModal,
+  eventArtistModal: EventArtistModal,
+  groupModal: GroupModal,
+  bookmarkFolderModal: BookmarkFolderModal,
+  bookmarkModal: BookmarkModal,
+  categoryModal: CategoryModal,
+  categorySelectModal: CategorySelectModal,
+  addressSearchModal: AddressSearchModal,
+};
 
 import { selectModalsSlice } from './modalsSlice';
 
