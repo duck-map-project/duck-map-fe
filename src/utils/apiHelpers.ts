20f5f344e99bb1c9fd22252c9cf894856d7260dc
vnfdusdl <rlachdus2@naver.com>
2023-11-18 17:23:02 +0900

    Feat: performApiAction 함수 생성 - api 비동기 함수 실행 및 에러처리 Refs: #339

---
+++ src/utils/apiHelpers.ts
@@ -0,0 +1,52 @@
+import {
+  MutationDefinition,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+  FetchArgs,
+} from '@reduxjs/toolkit/dist/query';
+import {
+  QueryReturnValue,
+  BaseQueryApi,
+} from '@reduxjs/toolkit/dist/query/baseQueryTypes';
+import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
+
+import { ArtistDataType, EditArtistDataType } from '../types/artistsType';
+
+import handleErrorResponse from './handleErrorResponse';
+
+type Data = ArtistDataType | EditArtistDataType;
+
+export type ApiFunction<T extends Data> = MutationTrigger<
+  MutationDefinition<
+    T,
+    (
+      args: string | FetchArgs,
+      api: BaseQueryApi,
+      extraOptions: object
+    ) => Promise<
+      QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
+    >,
+    any,
+    'api'
+  >
+>;
+
+export const performApiAction = async <T extends Data>(
+  data: T,
+  apiFunction: ApiFunction<T>,
+  extraFunction?: () => void,
+  successMessage?: string
+) => {
+  try {
+    const res = await apiFunction(data);
+
+    if ('data' in res) {
+      successMessage && alert(successMessage);
+      extraFunction && extraFunction();
+    } else if ('error' in res) {
+      handleErrorResponse(res.error);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
